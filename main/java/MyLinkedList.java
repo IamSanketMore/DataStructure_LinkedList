1cd8c7ac161e9d3f52cb3512491648049c3070a1
IamSanketMore <sanketmore.999.m@gmail.com>
2021-03-08 16:39:45 +0530

    Add pop() Method  for delete first Number linked list

--- main/java/MyLinkedList.java
+++ main/java/MyLinkedList.java
@@ -37,9 +37,13 @@ public class MyLinkedList {
         INode tempNode = myNode.getNext();
         myNode.setNext(newNode);
         newNode.setNext(tempNode);
-
     }
-
+    public INode pop()
+    {
+        INode tempNode= this.head;
+        this.head=head.getNext();
+        return tempNode;
+    }
 
     public void printMyNodes(){
         StringBuffer myNodes = new StringBuffer("my Nodes: ");
