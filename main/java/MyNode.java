dcea8ce20443699347802066f918b84da6f0d429
IamSanketMore <sanketmore.999.m@gmail.com>
2021-03-08 15:04:41 +0530

    Added : add numbers to linke list 56,30,70

--- main/java/MyNode.java
+++ main/java/MyNode.java
@@ -1,21 +1,30 @@
-public class MyNode <S>
-{
-    public S key;
-    public MyNode next;
+public class MyNode<K> implements INode<K>{
+    private K key;
+    private  INode next;
 
-    public MyNode(S key)
-    {
+
+
+    public MyNode(K key) {
         this.key =key;
         this.next=null;
     }
 
-    public MyNode getNext()
-    {
+
+    @Override
+    public K getKey() {
+        return key;
+    }
+
+    @Override
+    public void setKey(K key) {
+        this.key=key;
+    }
+
+    public INode getNext() {
         return next;
     }
 
-    public void setNext(MyNode next)
-    {
+    public void setNext(INode next) {
         this.next = next;
     }
 }
